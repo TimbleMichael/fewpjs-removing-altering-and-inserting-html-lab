06b18ceedfe7019549749f18c93630fdff888592
timble <timblemichael20@gmail.com>
2022-03-17 19:27:28 +1000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+// Removing a main ID from HTML
+const body = document.querySelector('#main');
+body.remove()
+
+let newHeader = document.createElement('h1')
+
+newHeader.setAttribute("id","victory")
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
